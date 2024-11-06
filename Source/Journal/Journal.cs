b5fc6c1f06e4857668128e3550ffb01e74c2e7e2
KonarJG <konarskikrzysztof1@gmail.com>
2024-11-06 14:33:55 +0100

    Finished documenting Journal.cs and Trap.cs

--- Source/Journal/Journal.cs
+++ Source/Journal/Journal.cs
@@ -15,12 +15,14 @@ public class Journal : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI ContentsText; //Miejsce na zawartoœæ notatki
 
-    private List<NoteUI> Notes = new List<NoteUI>();
-    private NoteUI CurrentNote;
-    public static Journal Instance;
+    private List<NoteUI> Notes = new List<NoteUI>(); //Lista notatek (ich interfejsów)
+    private NoteUI CurrentNote; //Aktualnie zaznaczona notatka (jej interfejs)
+    public static Journal Instance; //Globalny dostêp do dziennika
 
+    //Dodanie notatki
     public void AddNote(string name, string contents)
     {
+        //Spawnujemy interfejs notatki i ustawiamy mu odpowiedni stan nastêpnie dodajemy j¹ do listy
         var noteUI = Instantiate(NoteUIPrefab).GetComponent<NoteUI>();
         noteUI.transform.SetParent(JournalUI, false);
         noteUI.transform.position += new Vector3(0f, 10f, 0f);
@@ -29,18 +31,22 @@ public class Journal : MonoBehaviour
         noteUI.UpdateState();
     }
 
+    //Wybieranie aktualnie czytanej notatki
     public void SelectNote(int id)
     {
+        //Niepoprawne id
         if (id >= Notes.Count)
         {
             return;
         }
 
+        //Odznacz wszystkie notatki
         foreach (var note in Notes)
         {
             note.Unselect();
         }
 
+        //Zaznacz aktualn¹ notatkê i wyœwietl jej zawartoœæ
         Notes[id].Select();
         ContentsText.text = Notes[id].GetContents();
         CurrentNote = Notes[id];
@@ -52,8 +58,10 @@ public class Journal : MonoBehaviour
         Instance = this;
     }
 
+    //Interfejs tak jak kamerê w teorii powinno siê aktualizowaæ w LateUpdate
     private void LateUpdate()
     {
+        //jeœli nie mamy notatek to zresetuj stan dziennika
         if (Notes.Count == 0)
         {
             CurrentNote = null;
@@ -62,6 +70,7 @@ public class Journal : MonoBehaviour
             return;
         }
 
+        //Jeœli nie mamy ¿adnej wybranej notatki wybierz notatkê 1
         if (CurrentNote == null)
         {
             SelectNote(0);
--- Source/Traps/Trap.cs
+++ Source/Traps/Trap.cs
@@ -2,37 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Klasa obs³uguj¹ca rozstawion¹ pu³apkê, dziedziczy po WorldItem, poniewa¿ mo¿na j¹ podnieœæ tak jak przedmiot
 public class Trap : WorldItem
 {
     [SerializeField]
-    private ItemTrap TrapItem;
+    private ItemTrap TrapItem; //Przedmiot reprezentuj¹cy t¹ pu³apkê w ekwipunku
     [SerializeField]
-    private float GravityStrength;
+    private float GravityStrength; //Przyspieszenie grawitacyjne
 
     private void Awake()
     {
         
     }
 
+    //Aktywowanie pu³apki
     public void Activate()
     {
         GetComponent<SphereCollider>().enabled = true;
     }
 
+    //Jeœli przeciwnik wejdzie w t¹ pu³apkê uruchomi siê ta metoda
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Enemy" && !other.isTrigger)
         {
+            //Wywo³ujemy obs³ugê wejœcia w t¹ pu³apkê
             TrapItem.Trigger(this);
+            //usuwamy pu³apkê ze œwiata gry
             Destroy(gameObject);
         }
     }
 
 
+    //Aktualizujemy fizykê
     private void FixedUpdate()
     {
         var physics = GetComponent<CharacterController>();
 
+        //jeœli pu³apka nie jest na ziemi to nak³adamy grawitacjê
         if (!physics.isGrounded)
         {
             physics.Move(Physics.gravity.normalized * GravityStrength * Time.fixedDeltaTime);
